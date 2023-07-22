e486120c65464bb25157dd33bcaa15a952a85c74
Mihir Upadhyay <varahamihira2@gmail.com>
2023-07-22 13:11:44 +0530

    single train fix

--- Q2-FE/trains-app/src/App.js
+++ Q2-FE/trains-app/src/App.js
@@ -7,6 +7,7 @@ import AllTrains from './pages/AllTrains';
 import SingleTrain from './pages/SingleTrain';
 
 const App = () => {
+  const [dat, setDat] = React.useState()
   return (
     <Router>
       <AppBar position="static">
@@ -14,19 +15,21 @@ const App = () => {
           <Typography variant="h6">
             <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
               Trains Schedule App
+              <input type = "text" placeholder = "Enter Train No." name = "trainNo" onChange={(e) => {setDat(e.target.value)}}></input>
             </Link>
           </Typography>
         </Toolbar>
         <Tabs value={false} indicatorColor="secondary" centered>
           <Tab label="All Trains" component={Link} to="/" />
           {/* Replace 'trainId' with the appropriate parameter for SingleTrain page */}
-          <Tab label="Single Train" component={Link} to="/trains/trainId" />
+          <Tab label="Single Train" component={Link} to="/train/" />
         </Tabs>
       </AppBar>
       <Container>
         <Routes>
           <Route exact path="/" element={<AllTrains/>} />
-          <Route exact path="/train/" element={<SingleTrain id = {}/>} />
+          
+          <Route exact path="/train/" element={<SingleTrain id = {dat}/>} />
         </Routes>
       </Container>
     </Router>
